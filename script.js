dda4c5175c9819fb58fb4721da8257052e59d344
Ramji <ramjisanmugam@gmail.com>
2026-01-13 11:49:04 +0530

    Added count and filter for TaskCards

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 
-
 /* Hours Input*/
 const hoursInput = document.getElementById('hours')
 hoursInput.addEventListener("keydown",(e) => {
@@ -29,4 +28,59 @@ const navLinks = document.querySelector(".nav-links")
 
 menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("show")
+})
+
+
+/* Filter the navigation bar */
+
+const  navLabels = document.querySelectorAll('.nav-buttons label') 
+const taskBox = document.querySelectorAll('.active-task .content')
+
+// all-high-medium-low
+const allCount = document.getElementById('all-count')
+const highCount = document.getElementById('high-count')
+const mediumCount = document.getElementById('medium-count')
+const lowCount = document.getElementById('low-count')
+
+function updateCounts(){
+  let high = 0
+  let medium = 0
+  let low = 0
+
+  taskBox.forEach(function (cards){
+    if(cards.classList.contains('high'))
+      high++
+    else if(cards.classList.contains('medium'))
+      medium++
+    else if(cards.classList.contains('low'))
+      low++
+  })
+
+  allCount.textContent = high + medium + low  // adds (high+medium+low) = all
+  highCount.textContent = high
+  mediumCount.textContent = medium
+  lowCount.textContent = low
+}
+
+updateCounts()  // function called
+
+navLabels[0].classList.add('active')  // highlight color for only all
+
+
+navLabels.forEach(function(label){
+  label.onclick = function(){
+    var filter = this.dataset.filter   // -ex:label.dataset.filter = high,medium,low
+
+    navLabels.forEach(function (del) {
+      del.classList.remove('active')   // removes the navLabels[0] "active" class
+    })
+
+    this.classList.add('active')  // highlights only clicked filter
+
+    taskBox.forEach(function (cards) {
+      cards.style.display = 
+      filter === "all" || cards.classList.contains(filter)
+      ? 'block' : 'none'
+    })
+  }
 })
